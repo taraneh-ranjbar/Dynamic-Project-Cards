87f2c3992db44e979104a91baa3f736780e3e0d2
taraneh-ranjbar <taraneh.ranjbar@gmail.com>
2026-04-03 17:11:08 -0400

    refactor static cards into dynamic rendering using JavaScript

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const projects = [
     id: 1,
     title: "Digital Wallet System",
     description: "A secure digital wallet platform for managing user balances, transactions, and real-time payment tracking.",
+    button: "View Wallet Project",
     category: "fintech",
     image: "images/wallet.jpeg",
     link: "#"
@@ -11,6 +12,7 @@ const projects = [
     id: 2,
     title: "Personal Finance Manager (PFM)",
     description: "An intelligent finance dashboard that categorizes expenses, tracks income, and provides budgeting insights.",
+    button: "View PFM Project",
     category: "finance",
     image: "images/pfm.jpeg",
     link: "#"
@@ -19,14 +21,16 @@ const projects = [
     id: 3,
     title: "Payment Gateway Integration",
     description: "A backend-driven payment gateway system enabling secure online transactions with API-based processing.",
+    button: "View Payment Gateway Project",
     category: "payment",
-    image: "images/payment.jpg",
+    image: "images/payment.jpeg",
     link: "#"
   },
   {
     id: 4,
     title: "Fintech Web Dashboard",
     description: "A responsive web dashboard for monitoring financial data, analytics, and transaction reports.",
+    button: "View Fintech Web Project",
     category: "web",
     image: "images/dashboard.jpeg",
     link: "#"
@@ -35,9 +39,31 @@ const projects = [
     id: 5,
     title: "Transaction Monitoring Service",
     description: "A real-time service designed to detect suspicious financial activities and ensure secure transaction flows.",
+    button: "View TMS Project",
     category: "security",
-    image: "images/security.jpg",
+    image: "images/security.jpeg",
     link: "#"
   }
 ];
 console.log(projects);
+
+const container = document.getElementById("projects-container");
+function renderProjects(projectList) {
+  container.innerHTML = "";
+
+  projectList.forEach(project => {
+    const card = `
+      <article class="card">
+        <img src="${project.image}" alt="${project.title}">
+        <span class="badge">${project.category}</span>
+        <h3>${project.title}</h3>
+        <p>${project.description}</p>
+        <button>${project.button}</button>
+      </article>
+    `;
+
+    container.innerHTML += card;
+  });
+}
+
+renderProjects(projects);
